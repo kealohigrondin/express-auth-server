9081193039d114e934845f35efb74b4eb568b87b
Kealohi Grondin <jacob.grondin@wsu.edu>
2022-09-14 17:46:57 -0700

    updated connection string in config file

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ const morgan = require("morgan");
 const app = express();
 const router = require("./router");
 const mongoose = require("mongoose");
-
+const config = require("./config");
 //DB setup
-const connectionString =
-  "mongodb+srv://kealohigrondin:jkg1313@cluster0.usx9z5h.mongodb.net/?retryWrites=true&w=majority";
+
 mongoose
-  .connect(connectionString)
+  .connect(config.mongodbconnectionstring)
   .then(() => {
     const port = process.env.PORT || 3090; //use predefined port or 3090
     const server = http.createServer(app);
